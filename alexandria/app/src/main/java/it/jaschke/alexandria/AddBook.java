7f87b96b9caa87ec91ed74cdee3bc1f88fccc8ea
chipset <chipset95@yahoo.co.in>
2015-12-20 13:55:28 +0530

    Alexandria: No internet crash fix

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -9,7 +9,6 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
